c8f7716042ddc062888903cb375647a0bfe0a2a4
Vincent Velociter <vincent.velociter@gmail.com>
2012-05-16 16:20:03 +0200

    Add layout and assets

--- admin/views.py
+++ admin/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def index(request):
-    return HttpResponse('it works!');
+    return render_to_response('admin/index.html')
