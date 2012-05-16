8490a05ed8deed29850e3eceb07fd3021d33cdf7
Vincent Velociter <vincent.velociter@gmail.com>
2012-05-16 23:07:56 +0200

    List of paroles in index

--- admin/views.py
+++ admin/views.py
@@ -1,6 +1,8 @@
 # Create your views here.
-from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
+from parole2.models import Parole
+
 def index(request):
-    return render_to_response('admin/index.html')
+    paroles = Parole.objects.all()
+    return render_to_response('admin/index.html', {'paroles_to_publish': paroles})
