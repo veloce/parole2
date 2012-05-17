79d362811d67006b793115ad0ea1df1a49743f36
Vincent Velociter <vincent.velociter@gmail.com>
2012-05-17 16:12:01 +0200

    Filter are useless here

--- paroles/admin.py
+++ paroles/admin.py
@@ -1,28 +1,8 @@
-# -*- coding: utf-8 -*-
-
 from paroles.models import Parole
 from django.contrib import admin
-from django.contrib.admin import SimpleListFilter
-
-class IsPublishedListFilter(SimpleListFilter):
-    title = 'Etat'
-    parameter_name = 'published'
-
-    def lookups(self, request, model_admin):
-        return (
-            ('true', 'Publié'),
-            ('false', 'Non publié'),
-        )
-
-    def queryset(self, request, queryset):
-        if self.value() == 'true':
-            return Parole.objects.published()
-        if self.value() == 'false':
-            return Parole.objects.not_published()
 
 class ParoleAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'date')
-    list_filter = (IsPublishedListFilter,)
     actions = None
 
     def has_change_permission(self, request, obj=None):
@@ -34,5 +14,8 @@ class ParoleAdmin(admin.ModelAdmin):
         if obj is not None and obj.is_published():
             return False
         return True
+
+    def queryset(self, request):
+        return Parole.objects.not_published()
 
 admin.site.register(Parole, ParoleAdmin)
