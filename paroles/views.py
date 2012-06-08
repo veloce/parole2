9c87f1b6c1469bfd1f0c317bbb5f58977790e164
Vincent Velociter <vincent.velociter@gmail.com>
2012-06-08 17:35:45 +0200

    Add slug

--- paroles/views.py
+++ paroles/views.py
@@ -8,7 +8,7 @@ def index(request):
     parole = Parole.objects.last_published()
     return render_to_response('paroles/index.html', {'parole': parole})
 
-def parole(request, year, month, day):
+def parole(request, year, month, day, slug):
     date = tz.datetime(int(year), int(month), int(day)).date()
-    parole = get_object_or_404(Parole, date=date)
+    parole = get_object_or_404(Parole, date=date, slug=slug)
     return render_to_response('paroles/parole.html', {'parole': parole})
