952a85cdef6b1959b14965789c22e2cf250486c7
Vincent Velociter <vincent.velociter@gmail.com>
2012-06-13 00:48:24 +0200

    Prepare for heroku

--- parole2/settings.py
+++ parole2/settings.py
@@ -19,16 +19,9 @@ PAROLE2_DOMAIN = 'localhost:8000'
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'database.sqlite',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+# heroku db conf
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 # For a better security as we use openid auth with no persistence backend
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
@@ -152,7 +145,6 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
     'paroles',
     'admin',
-    'lettuce.django',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -183,3 +175,8 @@ LOGGING = {
         },
     }
 }
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
